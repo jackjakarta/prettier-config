e985c9384d6b128a03a7b95e4d75309a6ea48ef0
jackjakarta <al.termure@gmail.com>
2025-07-02 03:50:18 +0200

    feat: better library

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,4 +6,9 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   target: 'es2022',
   dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  treeshake: true,
 });
